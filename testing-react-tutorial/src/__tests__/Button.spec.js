b8ab86cd96b10adcd926a35ff738fb52dc79221a
jshabree <jshabree@emich.edu>
2020-11-17 10:13:15 -0500

    first tdd test

--- testing-react-tutorial/src/__tests__/Button.spec.js
+++ testing-react-tutorial/src/__tests__/Button.spec.js
@@ -3,17 +3,44 @@ import React from "react";
 // mounting component using "create"
 import { create } from "react-test-renderer";
 
-function Button(props) {
-    return <button> 
-        Nothing to do for now
-        </button>
-    
+class Button extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { text: ""};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(() => {
+            return { text: "PROCEED TO CHECKOUT" };
+        });
+    }
+    render() {
+        return (
+            <button onClick = {this.handleClick}>
+                {this.state.text || this.props.text}
+            </button>
+        );
+    }
 }
 
+
+// describe("Button component", () => {
+//     test('Matches the snapshot', () => {
+//         const button = create(<Button />);
+//         expect(button.toJSON()).toMatchSnapshot();
+//     });
+    
+// });
+
+// using react-test-renderer for asserting behavior on components
+// changing text on the button when its clicked
+
 describe("Button component", () => {
-    test('Matches the snapshot', () => {
-        const button = create(<Button />);
-        expect(button.toJSON()).toMatchSnapshot();
+    test('it shows the expected text when clicked (testing the wrong way)', () => {
+        const component = create(<Button text = "Subscribe to Basic" />);
+        const instance = component.getInstance();
+        expect(instance.state.text).toBe("");
     });
     
 });
