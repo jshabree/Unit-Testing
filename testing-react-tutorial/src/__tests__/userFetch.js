4a170dbed7258c9caa0aa3852d27dcfd78036935
jshabree <jshabree@emich.edu>
2020-11-17 10:59:37 -0500

    failing test

--- testing-react-tutorial/src/__tests__/userFetch.js
+++ testing-react-tutorial/src/__tests__/userFetch.js
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 
 let container;
 
 beforeEach(() => {
-    container = document.createElement("div");
-    document.body.appendChild(container);
-  });
-  
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
 afterEach(() => {
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
-  });
-  
-// testing below 
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+export default class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        // make sure to check for errors
+        return response.json();
+      })
+      .then(json => {
+        this.setState(() => {
+          return { data: json };
+        });
+      });
+  }
+  render() {
+    return (
+      <ul>
+        {this.state.data.map(user => (
+          <li key={user.name}>{user.name}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+// testing here
 
 describe("User component", () => {
     test("it shows a list of users", () => {
       act(() => {
-        render(<Users />, container);
+        render(<userFetch />, container);
       });
+      expect(container.textContent).toBe("What to expect?");
     });
   });
