21d4c7e65aa9cfb5955edb4eb8ae840b6a236e67
jshabree <jshabree@emich.edu>
2020-11-17 08:55:49 -0500

    adding snapshot feature

--- counter-app/src/App.test.js
+++ counter-app/src/App.test.js
@@ -1,9 +1,14 @@
-// test that app renders without crashing 
+// importing snapshot feature 
+
+import renderer from 'react-test-renderer';
 
 import React from 'react';
 import { shallow } from 'enzyme';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+// test that app renders without crashing 
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
