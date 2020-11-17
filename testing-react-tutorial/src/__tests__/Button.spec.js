fc50c26222b8aa3417a437f2785252fab699ed85
jshabree <jshabree@emich.edu>
2020-11-17 10:33:51 -0500

    modifying code for hooks

--- testing-react-tutorial/src/__tests__/Button.spec.js
+++ testing-react-tutorial/src/__tests__/Button.spec.js
@@ -1,27 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 // mounting component using "create"
 import { create } from "react-test-renderer";
 
-class Button extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = { text: ""};
-        this.handleClick = this.handleClick.bind(this);
+function Button(props) {
+    const [text, setText] = useState("");
+    function handleClick() {
+        setText("PROCEED TO CHECKOUT");
     }
 
-    handleClick() {
-        this.setState(() => {
-            return { text: "PROCEED TO CHECKOUT" };
-        });
-    }
-    render() {
-        return (
-            <button onClick = {this.handleClick}>
-                {this.state.text || this.props.text}
-            </button>
-        );
-    }
+    return <button onClick = {handleClick}>{text || props.text}
+    </button>
 }
 
 
