2162d07f5ce81f28456c727c6a75ee0f7e53dd76
jshabree <jshabree@emich.edu>
2020-11-17 08:42:03 -0500

    checking terminal logic

--- counter-app/src/App.test.js
+++ counter-app/src/App.test.js
@@ -2,28 +2,28 @@
 
 import React from 'react';
 import { shallow } from 'enzyme';
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
 
 // dummy test
 
-// describe('Addition', () => {
-//   it('knows that 2 and 2 make 4', () => {
-//     expect(2+2).toBe(4);
-//   });
-// });
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2+2).toBe(4);
+  });
+});
 
 // it and test are aliases
 
-// test('knows that 2 and 2 make 4', () => {
-//   expect(2 + 2).toBe(4);  // matcher function
-// });
+test('knows that 2 and 2 make 4', () => {
+  expect(2 + 2).toBe(4);  // matcher function
+});
 
 
 // testing app's initial state using shallow rendering
@@ -34,4 +34,16 @@ describe('App component', () => {
     const text = wrapper.find('p').text();
     expect(text).toEqual('Count: 0');
     });
+});
+
+// testing user's interaction to see if count is indeed incremented ( in this case, by 1)
+
+describe('App', () => {
+  it('increments count by 1 when button is clicked', () => {
+    const wrapper = shallow(<App />);
+    const incrementButton = wrapper.find('button.increment');
+    incrementButton.simulate('click');
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('Count: 1');
+  });
 });
