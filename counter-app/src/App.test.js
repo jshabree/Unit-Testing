63ee2e0b2cfd08cce67907f1a0901d17db207b62
jshabree <jshabree@emich.edu>
2020-11-17 08:23:35 -0500

    testing out counter component

--- counter-app/src/App.test.js
+++ counter-app/src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+// test that app renders without crashing 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+// dummy test
+
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2+2).toBe(4);
+  });
+});
+
+// it and test are aliases
+
+test('knows that 2 and 2 make 4', () => {
+  expect(2 + 2).toBe(4);
 });
